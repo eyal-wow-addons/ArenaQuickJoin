bbc8998ee0e96964510e02ea7c6e94f3f1aaa6a3
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-02-15 03:07:08 +0200

    Update the addon to have better in combat handling
    It's now needed to click the first time twice, the first time is to load the addon and the second time is to actually use it, this change was needed due to the combat changes I made.

--- ArenaQuickJoin.lua
+++ ArenaQuickJoin.lua
@@ -2,9 +2,15 @@ if UnitLevel("player") < GetMaxLevelForPlayerExpansion() then return end
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 frame:RegisterEvent("ADDON_LOADED")
 
-local ADDON_NAME = "Blizzard_PVPUI"
+local PVPUI_ADDON_NAME = "Blizzard_PVPUI"
+
+local function InCombat()
+    return InCombatLockdown() or UnitAffectingCombat("player")
+end
 
 local function CreateButton()
     local button = CreateFrame("Button", "ArenaQuickJoinMacroButton", UIParent, "SecureActionButtonTemplate, SecureHandlerStateTemplate, ActionButtonTemplate")
@@ -40,44 +46,32 @@ local function CreateButton()
     return button
 end
 
-local function SetGroupSize(button)
-    if GetNumSubgroupMembers(1) == 0 then
-        button:SetFrameRef("GroupSize", ConquestFrame.RatedSoloShuffle)
-    elseif GetNumSubgroupMembers(1) == 1 then
-        button:SetFrameRef("GroupSize", ConquestFrame.Arena2v2)
-    elseif GetNumSubgroupMembers(1) == 2 then
-        button:SetFrameRef("GroupSize", ConquestFrame.Arena3v3)
-    else
-        button:SetFrameRef("GroupSize", ConquestFrame.RatedBG)
-    end
-end
-
-local joinButton
+local joinMacroButton, configureMacroButton, setGroupSize
 frame:SetScript("OnEvent", function(self, eventName, ...)
     if eventName == "PLAYER_LOGIN" then
         ArenaQuickJoinDB = ArenaQuickJoinDB or {
             ["Position"] = {"CENTER", "CENTER", 0, 0}
         }
 
-        joinButton = CreateButton()
-        joinButton:SetTexture("achievement_bg_killxenemies_generalsroom")
-        joinButton:SetAttribute("type", "macro")
+        joinMacroButton = CreateButton()
+        joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+        joinMacroButton:SetAttribute("type", "macro")
 
-        do
-            local _, isLoaded = C_AddOns.IsAddOnLoaded(ADDON_NAME)
+        joinMacroButton:HookScript("OnClick", function(self)
+            local _, isLoaded = C_AddOns.IsAddOnLoaded(PVPUI_ADDON_NAME)
             if not isLoaded then
-                UIParentLoadAddOn(ADDON_NAME)
+                UIParentLoadAddOn(PVPUI_ADDON_NAME)
             end
-        end
+        end)
 
-        joinButton:SetScript("OnDragStart", function(self)
+        joinMacroButton:SetScript("OnDragStart", function(self)
             if not IsShiftKeyDown() then
                 return
             end
             self:StartMoving()
         end)
         
-        joinButton:SetScript("OnDragStop", function(self)
+        joinMacroButton:SetScript("OnDragStop", function(self)
             local point, _, relpoint, x, y = self:GetPoint()
             ArenaQuickJoinDB["Position"] = { point, relpoint, x, y }
             self:StopMovingOrSizing()
@@ -85,72 +79,90 @@ frame:SetScript("OnEvent", function(self, eventName, ...)
 
         do
             local point, relpoint, x, y = unpack(ArenaQuickJoinDB["Position"])
-            joinButton:ClearAllPoints()
-            joinButton:SetPoint(point, UIParent, relpoint, x, y)
+            joinMacroButton:ClearAllPoints()
+            joinMacroButton:SetPoint(point, UIParent, relpoint, x, y)
         end
     elseif eventName == "ADDON_LOADED" then
-        local addonName = ...
+        local arg1 = ...
         
-        if addonName ~= ADDON_NAME then
+        if arg1 ~= PVPUI_ADDON_NAME then
             return
         end
 
-        frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-        frame:RegisterEvent("PLAYER_REGEN_DISABLED")
-        frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-        frame:RegisterEvent("GROUP_ROSTER_UPDATE")
-        frame:RegisterEvent("MODIFIER_STATE_CHANGED")
+        configureMacroButton = function()
+            frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+            frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+            frame:RegisterEvent("MODIFIER_STATE_CHANGED")
 
-        joinButton:SetFrameRef("ConquestJoinButton", ConquestJoinButton)
-
-        SetGroupSize(joinButton)
-
-        do
-            local NO_OP_BUTTON = CreateFrame("Button", nil, nil, "SecureActionButtonTemplate")
-            hooksecurefunc("ConquestFrame_SelectButton", function(button)
-                if ConquestJoinButton:IsEnabled() then
-                    joinButton:SetFrameRef("SelectedButton", button)
+            setGroupSize = function(button)
+                if GetNumSubgroupMembers(1) == 0 then
+                    button:SetFrameRef("GroupSize", ConquestFrame.RatedSoloShuffle)
+                elseif GetNumSubgroupMembers(1) == 1 then
+                    button:SetFrameRef("GroupSize", ConquestFrame.Arena2v2)
+                elseif GetNumSubgroupMembers(1) == 2 then
+                    button:SetFrameRef("GroupSize", ConquestFrame.Arena3v3)
                 else
-                    joinButton:SetFrameRef("SelectedButton", NO_OP_BUTTON)
+                    button:SetFrameRef("GroupSize", ConquestFrame.RatedBG)
                 end
-            end)
+            end
+
+            joinMacroButton:SetFrameRef("ConquestJoinButton", ConquestJoinButton)
+
+            setGroupSize(joinMacroButton)
+
+            do
+                local NO_OP_BUTTON = CreateFrame("Button", nil, nil, "SecureActionButtonTemplate")
+                hooksecurefunc("ConquestFrame_SelectButton", function(button)
+                    if InCombat() then return end
+                    if ConquestJoinButton:IsEnabled() then
+                        joinMacroButton:SetFrameRef("SelectedButton", button)
+                    else
+                        joinMacroButton:SetFrameRef("SelectedButton", NO_OP_BUTTON)
+                    end
+                end)
+            end
+
+            SecureHandlerWrapScript(joinMacroButton, "OnClick", joinMacroButton, [[
+                if IsShiftKeyDown() then
+                    self:SetAttribute("macrotext", "")
+                    return
+                end
+
+                local SelectedButton = self:GetFrameRef("SelectedButton")
+                local GroupSize = self:GetFrameRef("GroupSize")
+
+                if IsAltKeyDown() then
+                    self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton1")
+                elseif GroupSize ~= SelectedButton or IsControlKeyDown() then
+                    self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton2")
+                else
+                    self:SetAttribute("macrotext", "/click ConquestJoinButton")
+                end
+            ]])
+
+            configureMacroButton = nil
         end
 
-        SecureHandlerWrapScript(joinButton, "OnClick", joinButton, [[
-            if IsShiftKeyDown() then
-                self:SetAttribute("macrotext", "")
-                return
-            end
-
-            local SelectedButton = self:GetFrameRef("SelectedButton")
-            local GroupSize = self:GetFrameRef("GroupSize")
-
-            if IsAltKeyDown() then
-                self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton1")
-            elseif GroupSize ~= SelectedButton or IsControlKeyDown() then
-                self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton2")
-            else
-                self:SetAttribute("macrotext", "/click ConquestJoinButton")
-            end
-        ]])
-    elseif eventName == "GROUP_ROSTER_UPDATE" then
-        SetGroupSize(joinButton)
+        if not InCombat() then configureMacroButton() end
+    elseif eventName == "GROUP_ROSTER_UPDATE" and setGroupSize then
+        setGroupSize(joinMacroButton)
     elseif eventName == "PLAYER_ENTERING_WORLD" then
         if IsInInstance() then
-            joinButton:DisableWithStyle("hide")
+            joinMacroButton:DisableWithStyle("hide")
         else
-            joinButton:EnableWithStyle("show")
+            joinMacroButton:EnableWithStyle("show")
         end
     elseif eventName == "PLAYER_REGEN_DISABLED" then
-        joinButton:DisableWithStyle("grayout")
+        joinMacroButton:DisableWithStyle("grayout")
     elseif eventName == "PLAYER_REGEN_ENABLED" then
-        joinButton:EnableWithStyle("normal")
+        if configureMacroButton then configureMacroButton() end
+        joinMacroButton:EnableWithStyle("normal")
     elseif eventName == "MODIFIER_STATE_CHANGED" then
         local key, down = ...
         if down == 1 and (key == "LALT" or key == "RALT") then
-            joinButton:SetTexture("achievement_bg_winwsg")
+            joinMacroButton:SetTexture("achievement_bg_winwsg")
         else
-            joinButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+            joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
         end
     end
 end)
